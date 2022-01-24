c5e42de68200a873da51043f6acc3b89d87042ee
zhuxianglong <56494565@qq.com>
2022-01-24 18:32:42 +0800

    env

--- cmd/env.go
+++ cmd/env.go
@@ -1,14 +1,34 @@
 package cmd
 
 import (
+	"bytes"
 	"fmt"
 	"github.com/urfave/cli/v2"
+	"log"
+	"os/exec"
 )
 
 type Env struct {
 }
 
 func (e Env) Action(c *cli.Context) error {
-	fmt.Println(c.Args().First())
+	cmd := exec.Command("cmd")
+	in := bytes.NewBuffer(nil)
+	cmd.Stdin = in
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	go func() {
+		// start stop restart
+		in.WriteString("go env\n") //写入你的命令，可以有多行，"\n"表示回车
+	}()
+	if err := cmd.Start(); err != nil {
+		log.Println(err)
+	}
+
+	if err := cmd.Wait(); err != nil {
+		log.Println(err)
+	}
+	rt := out.String()
+	fmt.Println(rt)
 	return nil
 }
