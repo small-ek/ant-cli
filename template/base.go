bc5e3345440bceb92ac3f285a793dd79fc9177cd
zhuxianglong <56494565@qq.com>
2024-12-04 18:23:01 +0800

    修复base错误

--- template/base.go
+++ template/base.go
@@ -5,7 +5,7 @@ import (
 )
 
 func Base(name string) string {
-	return fmt.Sprintf("package vo\n\nimport (\n\t\"github.com/gin-gonic/gin\"\n\t\"github.com/small-ek/antgo/os/alog\"\n\t\"github.com/small-ek/antgo/os/config\"\n\t\"github.com/small-ek/antgo/utils/response\"\n\t\"go.uber.org/zap\"\n\t\"server/app/entity/models\"\n)\n\ntype Base struct {\n\tCode int `json:\"code\"` //Status Code\n}\n\n// Success 成功返回\nfunc (b *Base) Success(c *gin.Context, msg string, data ...interface{}) {\n\tcode := 0\n\tif b.Code != 0 {\n\t\tcode = b.Code\n\t}\n\n\tc.SecureJSON(200, response.Success(msg, code, data...))\n}\n\n// Fail 错误返回\nfunc (b *Base) Fail(c *gin.Context, msg string, err ...error) {\n\tif len(err) > 0 {\n\t\talog.Write.Error(\"Return error\", zap.Errors(\"Fail Error\", err))\n\t}\n\tcode := 1\n\tif b.Code != 0 {\n\t\tcode = b.Code\n\t}\n\tif config.GetBool(\"system.debug\") == true {\n\t\tc.SecureJSON(200, response.Fail(msg, code, err[0].Error()))\n\t\treturn\n\t}\n\tc.SecureJSON(200, response.Fail(msg, code))\n\n}\n\n// SetCode 修改状态\nfunc (b *Base) SetCode(code int) *Base {\n\tb.Code = code\n\treturn b\n}\n\n// Page 分页数据\nfunc (b *Base) Page(total int64, list interface{}) response.Page {\n\treturn response.Page{\n\t\tTotal: total,\n\t\tItems: list,\n\t}\n}\n\n\n// GetDeviceId 获取设备号\nfunc (b *Base) GetDeviceId(c *gin.Context) string {\n\treturn c.GetHeader(\"device-id\")\n}\n")
+	return fmt.Sprintf("package vo\n\nimport (\n\t\"github.com/gin-gonic/gin\"\n\t\"github.com/small-ek/antgo/os/alog\"\n\t\"github.com/small-ek/antgo/os/config\"\n\t\"github.com/small-ek/antgo/utils/response\"\n\t\"go.uber.org/zap\"\n)\n\ntype Base struct {\n\tCode int `json:\"code\"` //Status Code\n}\n\n// Success 成功返回\nfunc (b *Base) Success(c *gin.Context, msg string, data ...interface{}) {\n\tcode := 0\n\tif b.Code != 0 {\n\t\tcode = b.Code\n\t}\n\n\tc.SecureJSON(200, response.Success(msg, code, data...))\n}\n\n// Fail 错误返回\nfunc (b *Base) Fail(c *gin.Context, msg string, err ...error) {\n\tif len(err) > 0 {\n\t\talog.Write.Error(\"Return error\", zap.Errors(\"Fail Error\", err))\n\t}\n\tcode := 1\n\tif b.Code != 0 {\n\t\tcode = b.Code\n\t}\n\tif config.GetBool(\"system.debug\") == true {\n\t\tc.SecureJSON(200, response.Fail(msg, code, err[0].Error()))\n\t\treturn\n\t}\n\tc.SecureJSON(200, response.Fail(msg, code))\n\n}\n\n// SetCode 修改状态\nfunc (b *Base) SetCode(code int) *Base {\n\tb.Code = code\n\treturn b\n}\n\n// Page 分页数据\nfunc (b *Base) Page(total int64, list interface{}) response.Page {\n\treturn response.Page{\n\t\tTotal: total,\n\t\tItems: list,\n\t}\n}\n\n// GetDeviceId 获取设备号\nfunc (b *Base) GetDeviceId(c *gin.Context) string {\n\treturn c.GetHeader(\"device-id\")\n}\n")
 	//package vo
 
 	//import (
@@ -14,7 +14,6 @@ func Base(name string) string {
 	//"github.com/small-ek/antgo/os/config"
 	//"github.com/small-ek/antgo/utils/response"
 	//"go.uber.org/zap"
-	//"server/app/entity/models"
 	//)
 	//
 	//type Base struct {
