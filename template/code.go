e8eb355099ad9f8e8b9457cfcec7a4594d9083f7
zhuxianglong <56494565@qq.com>
2025-01-12 16:03:32 +0800

    no message

--- template/code.go
+++ template/code.go
@@ -4,32 +4,50 @@ func Code(name string) string {
 	return `
 package vo
 
-// INVALID_REQUEST_PARAMETERS 无效的请求参数
-const INVALID_REQUEST_PARAMETERS = "INVALID_REQUEST_PARAMETERS"
+// Code 业务状态码
+const (
+	INVALID_REQUEST_PARAMETERS = "INVALID_REQUEST_PARAMETERS" // INVALID_REQUEST_PARAMETERS 无效的请求参数
 
-// SUCCESS 成功
-const SUCCESS = "SUCCESS"
+	SUCCESS = "SUCCESS" // SUCCESS 成功
 
-// FAILED 失败
-const FAILED = "FAILED"
+	FAILED = "FAILED" // FAILED 失败
 
-// CREATION_FAILED 创建失败
-const CREATION_FAILED = "CREATION_FAILED"
+	CREATION_FAILED = "CREATION_FAILED" // CREATION_FAILED 创建失败
 
-// CREATION_SUCCESS 创建成功
-const CREATION_SUCCESS = "CREATION_SUCCESS"
+	CREATION_SUCCESS = "CREATION_SUCCESS" // CREATION_SUCCESS 创建成功
 
-// UPDATE_FAILED 更新失败
-const UPDATE_FAILED = "UPDATE_FAILED"
+	UPDATE_FAILED = "UPDATE_FAILED" // UPDATE_FAILED 更新失败
 
-// UPDATE_SUCCESS 更新成功
-const UPDATE_SUCCESS = "UPDATE_SUCCESS"
+	UPDATE_SUCCESS = "UPDATE_SUCCESS" // UPDATE_SUCCESS 更新成功
 
-// DELETE_FAILED 删除失败
-const DELETE_FAILED = "DELETE_FAILED"
+	DELETE_FAILED = "DELETE_FAILED" // DELETE_FAILED 删除失败
 
-// DELETE_SUCCESS 删除成功
-const DELETE_SUCCESS = "DELETE_SUCCESS"
+	DELETE_SUCCESS = "DELETE_SUCCESS" // DELETE_SUCCESS 删除成功
+)
+
+// 全局映射表
+var Messages = map[string]string{
+	"INVALID_REQUEST_PARAMETERS": "请求参数无效",
+	"SUCCESS":                    "操作成功",
+	"FAILED":                     "操作失败",
+
+	"CREATION_FAILED":  "创建操作失败",
+	"CREATION_SUCCESS": "创建操作成功",
+
+	"UPDATE_FAILED":  "更新操作失败",
+	"UPDATE_SUCCESS": "更新操作成功",
+
+	"DELETE_FAILED":  "删除操作失败",
+	"DELETE_SUCCESS": "删除操作成功",
+}
+
+func GetMessage(code string) string {
+	if msg, exists := Messages[code]; exists {
+		return msg
+	}
+	return "未知错误，请联系客服解决。"
+}
+
 
 `
 
